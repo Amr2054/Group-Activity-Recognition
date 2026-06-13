Fixed Dynamic Weights Calculation Function

models/baseline_3/train_phase_A.py
@@ -86,8 +86,8 @@ if __name__ == "__main__":
     print_model_summary(model)
 
     # Dynamic Class Weighting
-    logger.info("Calculating dynamic class weights to combat imbalance")
-    class_weights = calculate_balanced_weights(train_set=train_set,min_length=config.model['num_classes'])
+    logger.info("Calculating dynamic class weights")
+    class_weights = calculate_balanced_weights(train_set=train_set,min_length=config.model['num_classes'],sample_index=2)
     weights_tensor = torch.FloatTensor(class_weights).to(device)
 
     logger.info(f"Applied Class Weights: {np.round(class_weights, 3)}")

models/baseline_5/train_phase_A.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
 
     # Dynamic Class Weighting
     logger.info("Calculating dynamic class weights from the dataset")
-    class_weights = calculate_balanced_weights(train_set=train_set,min_length=config.model['num_classes'])
+    class_weights = calculate_balanced_weights(train_set=train_set,min_length=config.model['num_classes'],sample_index=1)
     weights_tensor = torch.FloatTensor(class_weights).to(device)
 
     logger.info(f"Applied Class Weights: {np.round(class_weights, 3)}")

utils/helper.py
@@ -57,9 +57,9 @@ def setup_logger(run_dir):
 
     return logger
 
-def calculate_balanced_weights(train_set,min_length):
+def calculate_balanced_weights(train_set,min_length,sample_index):
 
-    all_train_labels = [sample[1] for sample in train_set.samples]
+    all_train_labels = [sample[sample_index] for sample in train_set.samples]
     class_counts = np.bincount(all_train_labels, minlength=min_length)
     total_samples = len(all_train_labels)
     num_classes_active = len([c for c in class_counts if c > 0])